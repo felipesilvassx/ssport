a6a816b892e25d462369c89ca9800478b0e3fde1
felipe silva <felipesilvassunix@tuta.io>
2024-08-26 17:12:10 -0300

    novo update

--- ipv4.h
+++ ipv4.h
@@ -15,16 +15,35 @@
 
     void ipv4Scan(char *ip, int proto){
         struct sockaddr_in addr;
-        int cnx = 0, sock = 0;
+        char buff[256];
+        int cnx = 0, sock = 0, rc = 0;
         for(int loop=0; loop<= 65535; loop++){
             addr.sin_family = AF_INET;
             addr.sin_addr.s_addr = inet_addr(ip);
             addr.sin_port = htons(loop);
+            //tcp
             if(proto == 0){
                 sock = socket(AF_INET, SOCK_STREAM, 0);
-            }
+                if(sock < 0){
+                    perror("socket error");
+                    exit(EXIT_FAILURE);
+                }
+                cnx = connect(sock, (struct sockaddr *)&addr, sizeof(addr));
+                if(cnx == 0){
+                    printf("*Porta => %d [tcp] aberta...\n", loop);
+                }
+            }//udp
             else if(proto == 1){
+                char *msg = "HTTP/1.0";
                 sock = socket(AF_INET, SOCK_DGRAM, 0);
+                if(sock < 0){
+                    perror("socket error");
+                    exit(EXIT_FAILURE);
+                }
+                cnx = sendto(sock, msg, strlen(msg), 0, (struct sockaddr*)&addr, sizeof(addr));
+                if(cnx > 0){
+                    printf("*Porta => %d [udp] aberta...\n", loop);
+                }
             }
             else{
                 printf("=== SSPort v1.0 by levi_jr ===\n");
@@ -33,16 +52,6 @@
                 printf("ou: ./ssport 127.0.0.1 udp 4 log.txt\n");
                 printf("use 4 para ipv4 ou 6 para ipv6\n");
             }
-            if(sock < 0){
-                perror("socket error");
-                exit(EXIT_FAILURE);
-            }
-            cnx = connect(sock, (struct sockaddr *)&addr, sizeof(addr));
-            if(cnx == 0){
-                printf("*Porta => %d aberta...\n", loop);
-            }
-
-            close(cnx);
             close(sock);
             
 
